a7372d9cb534ce28e005e89ffc38a8674bbf695e
JuanOctavioCastrogiovanni <jcastrogiovanni@frba.utn.edu.ar>
2023-10-03 11:10:29 -0300

    add typeorm configuration and install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PartnerModule } from './partner/partner.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PartnerModule],
+  imports: [PartnerModule, ConfigModule.forRoot(),
+    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'partners_module',
+      username: 'root',
+      password: '',      
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
