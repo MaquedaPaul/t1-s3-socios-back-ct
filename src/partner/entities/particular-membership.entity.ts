7f116393ff9806eac00e0457469983125251e1c9
Rodrigo Gutson <82126714+RodrigoGutson@users.noreply.github.com>
2023-10-03 15:47:34 -0300

    membership and particularmembership created

--- src/partner/entities/particular-membership.entity.ts
+++ src/partner/entities/particular-membership.entity.ts
@@ -1,1 +1,44 @@
-export class ParticularMembershipEntity {}
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Persistence } from "./persistence.entity";
+import { MembershipEntity } from "./membership.entity";
+import { Partner } from "./partner.entity";
+
+
+@Entity('particular_memberships')
+export class ParticularMembershipEntity extends Persistence{
+
+    @ManyToOne(() => MembershipEntity, { eager: true })
+    @JoinColumn({ name: 'id_membership' })
+    membership: MembershipEntity;
+
+    @Column({ name: 'start_date', type: 'date' })
+    startDate: Date;
+
+    @Column({ name: 'value', type: 'double' })
+    value: number;
+
+    @ManyToOne(() => Partner, { eager: true })
+    @JoinColumn({ name: 'id_partner' })
+    partner: Partner;
+
+
+    isActive(): boolean {
+        const currentDate = new Date();
+        return currentDate > this.startDate && currentDate < this.getEndDate();
+      }
+
+    getEndDate(): Date {
+        const endDate = new Date(this.startDate);
+        endDate.setMonth(endDate.getMonth() + this.membership.monthDuration);
+        return endDate;
+    }
+
+
+    constructor(membership: MembershipEntity, startDate: Date, value: number) {
+        super();
+        this.membership = membership;
+        this.startDate = startDate;
+        this.value = value;
+        
+    }
+}
