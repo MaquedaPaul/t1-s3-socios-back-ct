e185f344cf39432e92b7a957ce5a1f296b3f13fb
JuanOctavioCastrogiovanni <jcastrogiovanni@frba.utn.edu.ar>
2023-10-04 09:55:15 -0300

    LocationDTO created

--- src/partner/dto/location-dto.decorator.ts
+++ src/partner/dto/location-dto.decorator.ts
@@ -1,3 +1,28 @@
-import { SetMetadata } from '@nestjs/common';
+import { IsString, MinLength } from "class-validator";
 
-export const LocationDto = (...args: string[]) => SetMetadata('location-dto', args);
+
+
+export class LocationDTO {
+
+    @IsString()
+    @MinLength(3)
+    street: string;
+
+    @IsString()
+    @MinLength(2)
+    streetAdress: string;
+
+    @IsString()
+    floor: string;
+
+    @IsString()
+    apartment: string;
+
+    @IsString()
+    department: string;
+
+    @IsString()
+    @MinLength(3)
+    province: string;
+
+}
