628de973b1ead65f2b6bf7625c0ad548dffe9563
MaquedaPaul <maquedapablodavid@gmail.com>
2023-10-04 19:18:18 -0300

    Partner service changes (needs fixes) -New imports -createPartner now is create -Phones, emails, websites creations now are implemented -Added function to convert numbers to enum phone types -findOne method that has been deleted by mistake now is back again but not implemented -getCategoriesByIds is now implemented

--- src/partner/services/partner.service.ts
+++ src/partner/services/partner.service.ts
@@ -3,17 +3,20 @@ import { CreatePartnerDto } from '../dto/create-partner.dto';
 import { UpdatePartnerDTO } from '../dto/update-partner.dto';
 import { Liquid } from 'liquidjs';
 
-import { Membership, Category, Partner } from '../entities/package.entities';
+import { Membership, Category, Partner, PartnerEmail } from '../entities/package.entities';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PartnerType } from '../entities/partner.entity';
 import { ParticularMembership } from '../entities/particular-membership.entity';
-import { Phone } from '../entities/phone.entity';
+import { Phone, PhoneType } from '../entities/phone.entity';
 import { Location } from '../entities/location.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { CategoryDTO } from '../dto/category.dto';
+import { PartnerWebsite } from '../entities/website.entity';
 
 
 @Injectable()
 export class PartnerService {
+
  
 
   private readonly engine:Liquid
@@ -52,7 +55,7 @@ export class PartnerService {
     });
   }
 
-   async createPartner(partner: CreatePartnerDto): Promise<boolean>{
+   async create(partner: CreatePartnerDto): Promise<boolean>{
     try {
       const partnerType =
         partner.partnerType === '0' ? 'PLENARY' : 'ASSOCIATE';
@@ -64,6 +67,7 @@ export class PartnerService {
         partnerType: partnerType as PartnerType,
       });
 
+
       const location = this.locationRepository.create({
         street: partner.street,
         streetAddress: partner.streetAddress,
@@ -71,38 +75,51 @@ export class PartnerService {
         apartment: partner.apartment,
         department: partner.department,
         province: partner.province,
-         //No se por qué no funciona
+        
       });
       await this.locationRepository.save(location);
-/*
+      
       partner.phones.forEach(async (phone) => {
-        const newPhone = this.phoneRepository.create({
+
+        const aPhoneType = this.conversionEnumPhoneType(phone.type);
+        const newPhone =
+          this.phoneRepository.create({
           areaCode: phone.areaCode,
           number: phone.number,
-          type: phone.type,
-        });
+            type: aPhoneType,
+            partner: newPartner,
+          });
+        newPhone.partner = newPartner;
         await this.phoneRepository.save(newPhone);
         newPartner.phones.push(newPhone);
       });
-*/
 
-      const membership = await this.membershipRepository.findOneBy({ id: partner.membershipID}
+
+      const membership = await this.membershipRepository.findOneBy({
+        id: partner.membershipID
+      }
       );
 
       const particularMembership = this.particularMembershipRepository.create({
+        membership: membership,
         startDate: new Date(partner.startDate),
         value: partner.membershipValue,
+        partner: newPartner,
       });
+
       await this.membershipRepository.save(particularMembership);
       newPartner.memberships.push(particularMembership);
 
       newPartner.location = location;
-      //newPartner.categories = await this.getCategoriesByIds(partner.categories);
-      // newPartner.emails = partner.emails;
-      // newPartner.webSites = partner.webSites;
-
+      newPartner.categories = await this.getCategoriesByIds(partner.categories);
+      newPartner.emails = partner.emails.
+        map(email => new PartnerEmail(email));
+      //newPartner.emails.forEach(email => email.partner = newPartner);
+      newPartner.websites = partner.websites.map(website => new PartnerWebsite(website));
+      //newPartner.websites.forEach(website => website.partner = newPartner);
       await this.partnerRepository.save(newPartner);
-      return true;
+      const partners = await this.partnerRepository.find();
+      return this.dataPrint(partners,``, "home")  
     } catch (error) {
       throw new Error('Error al crear el socio, por favor inténtelo más tarde');
     }
@@ -115,9 +132,25 @@ export class PartnerService {
   //     message: "" 
   //  });
   }
+  private conversionEnumPhoneType(phoneType: number) {
 
+    switch (phoneType) {
+      case 0:
+        return PhoneType.FAX;
+      case 1:
+        return PhoneType.OFFICE;
+      case 2:
+        return PhoneType.MOBILE;
+      default:
+        throw new Error("Tipo de numero no reconocido");
+    }
+  }
+
+    findOne(arg0: number) {
+    throw new Error('Method not implemented.');
+  }
   async findAll() {
-
+    
     this.categories = await this.categoryRepository.find();
     this.membership = await this.membershipRepository.find();
 
@@ -131,13 +164,9 @@ export class PartnerService {
     //                                           });
   }
 
-  async findMembershipById(id: number) {
-    const membership = await this.membershipRepository.findOneBy({id});
-    return membership;
-  }
-  async getCategoriesByIds(idsCategories: number[]): Promise<Category[]> {
+  async getCategoriesByIds(idsCategories: CategoryDTO[]): Promise<Category[]> {
     // Utiliza el método 'findByIds' del repositorio para buscar categorías por sus IDs
-    return this.categoryRepository.findByIds(idsCategories);
+    return this.categoryRepository.findBy({ id: In(idsCategories) });
   }
 
   async update(id: number, updatePartnerDto: UpdatePartnerDTO) {
