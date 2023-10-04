a02febc97eafac4a5a20adae04bce6212d715eab
MaquedaPaul <maquedapablodavid@gmail.com>
2023-10-04 14:41:02 -0300

    partner dto now has its attributes

--- src/partner/dto/create-partner.dto.ts
+++ src/partner/dto/create-partner.dto.ts
@@ -1,1 +1,74 @@
-export class CreatePartnerDto {}
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreatePartnerDto {
+  @IsNotEmpty()
+  @IsString()
+  denominacion: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  street: string;
+
+  @IsOptional()
+  @IsString()
+  image: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  streetAddress: string;
+
+  @IsOptional()
+  @IsString()
+  floor: string;
+
+  @IsOptional()
+  @IsString()
+  apartment: string;
+
+  @IsOptional()
+  @IsString()
+  department: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  province: string;
+
+//   @IsOptional()
+//   @IsArray()
+//   phones: PhoneDTO[];
+
+  @IsOptional()
+  @IsArray()
+  emails: string[];
+
+  @IsOptional()
+  @IsArray()
+  webSites: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  partnerType: string;
+
+  @IsOptional()
+  @IsArray()
+  categories: number[];
+
+  @IsNotEmpty()
+  @IsNumber()
+  membershipValue: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  membershipID: number;
+
+  @IsNotEmpty()
+  @IsString()
+  startDate: string;
+}
