a44ede5957cbf4c7517354e77f099960332c40bc
Rodrigo Gutson <82126714+RodrigoGutson@users.noreply.github.com>
2023-10-04 14:30:35 -0300

    updatePartnerDTO created

--- src/partner/dto/update-partner.dto.ts
+++ src/partner/dto/update-partner.dto.ts
@@ -1,10 +1,27 @@
+import { IsString } from "class-validator";
+
 export class UpdatePartnerDTO {
+    @IsString()
     denomination: string;
+
+    @IsString()
     name: string;
+
+    @IsString()
     street: string;
+
+    @IsString()
     streetAddress: string;
+    
+    @IsString()
     floor: string;
+    
+    @IsString()
     apartment: string;
+    
+    @IsString()
     department: string;
+    
+    @IsString()
     province: string;
 }
