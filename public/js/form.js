df7374b23f87a9bf8df5d521af338da447dac241
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2023-10-05 15:30:15 -0300

    feat: add buttons

---
+++ public/js/form.js
@@ -0,0 +1,6 @@
+
+
+function mostrarForm() {
+    const form = document.getElementById("addAssociate");
+    form.style.transform = form.style.transform === "translate(100%)" ? "translate(0)" : "translate(100%)";
+}
