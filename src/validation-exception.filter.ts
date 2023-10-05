c58c1a89d53093072ebd46a280a68cd98978d894
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2023-10-05 16:11:09 -0300

    manejo de excepciones

---
+++ src/validation-exception.filter.ts
@@ -0,0 +1,37 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import { ValidationError } from 'class-validator';
+
+@Catch(HttpException)
+export class ValidationExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    if (exception instanceof HttpException) {
+      const validationErrors = exception.getResponse() as ValidationError[];
+      const messages = validationErrors.map((error) => {
+        const constraints = error.constraints;
+        if (constraints)
+          return constraints[Object.keys(constraints)[0]];
+      });
+
+      response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error: 'Solicitud Inválida',
+        message: messages,
+      });
+    } else {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error: 'Error interno del servidor',
+        message: 'Algo salio mal.',
+      });
+    }
+  }
+}
