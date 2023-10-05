b91cbac67efa71dd3ffcd2062fde66ac5362661d
fedealvarenga <82116673+fedealvarenga@users.noreply.github.com>
2023-10-05 16:56:58 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [PartnerModule, ConfigModule.forRoot(),
     
     TypeOrmModule.forRoot({
-     // type: 'mysql',
-     // host: 'dokku-mariadb-acc-squad-t1s3-db-dev',
-     /* port: 3306,
+      type: 'mysql',
+      host: 'dokku-mariadb-acc-squad-t1s3-db-dev',
+      port: 3306,
       database: 'acc_squad_t1s3_db_dev',
       username: 'root',
       password: 'Fr3a951',      
       autoLoadEntities: true,
-      synchronize: true,*/
-    url: 'mysql://root:Fr3a951@dokku-mariadb-acc-squad-t1s3-db-dev:3306/acc_squad_t1s3_db_dev',
+      synchronize: true,
+   // url: 'mysql://root:Fr3a951@dokku-mariadb-acc-squad-t1s3-db-dev:3306/acc_squad_t1s3_db_dev',
       synchronize: true,
     
     })
