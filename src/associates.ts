d9b3510876d1de990068835df26bb5d1ad8ba30b
Valentin Román Aquino <vaquino@frba.utn.edu.ar>
2023-10-05 17:46:56 -0300

    feat(public): add associate page

---
+++ src/associates.ts
@@ -0,0 +1,178 @@
+export const partners: any = [
+	{
+		denomination: 'Socio A',
+		name: 'Nombre A',
+		image: 'https://www.ejemplo.com/imagen1.jpg',
+		location: {
+			street: 'Calle 1',
+			streetAdress: 'Dirección 1',
+			floor: 'Piso 1',
+			apartment: 'Apartamento 1',
+			department: 'Departamento 1',
+			province: 'Provincia 1'
+		},
+		memberships: [
+			{
+				name: 'Membresía 1A',
+				monthDuration: 6
+			},
+			{
+				name: 'Membresía 2A',
+				monthDuration: 12
+			}
+		],
+		partnerType: 'Tipo A',
+		categories: [
+			{
+				name: 'Categoría A1',
+				description: 'Descripción A1'
+			},
+			{
+				name: 'Categoría A2',
+				description: 'Descripción A2'
+			}
+		],
+		phones: [
+			{
+				phoneNumber: '123-456-7890'
+			},
+			{
+				phoneNumber: '987-654-3210'
+			}
+		],
+		emails: [
+			{
+				email: 'correo1A@example.com'
+			},
+			{
+				email: 'correo2A@example.com'
+			}
+		],
+		websites: [
+			{
+				website: 'https://www.ejemplo1A.com'
+			},
+			{
+				website: 'https://www.ejemplo2A.com'
+			}
+		]
+	},
+	{
+		denomination: 'Socio B',
+		name: 'Nombre B',
+		image: 'https://www.ejemplo.com/imagen2.jpg',
+		location: {
+			street: 'Calle 2',
+			streetAdress: 'Dirección 2',
+			floor: 'Piso 2',
+			apartment: 'Apartamento 2',
+			department: 'Departamento 2',
+			province: 'Provincia 2'
+		},
+		memberships: [
+			{
+				name: 'Membresía 1B',
+				monthDuration: 6
+			}
+		],
+		partnerType: 'Tipo B',
+		categories: [
+			{
+				name: 'Categoría B1',
+				description: 'Descripción B1'
+			}
+		],
+		phones: [
+			{
+				phoneNumber: '111-222-3333'
+			}
+		],
+		emails: [
+			{
+				email: 'correoB@example.com'
+			}
+		],
+		websites: [
+			{
+				website: 'https://www.ejemploB.com'
+			}
+		]
+	},
+	{
+		denomination: 'Socio C',
+		name: 'Nombre C',
+		image: 'https://www.ejemplo.com/imagen3.jpg',
+		location: {
+			street: 'Calle 3',
+			streetAdress: 'Dirección 3',
+			floor: 'Piso 3',
+			apartment: 'Apartamento 3',
+			department: 'Departamento 3',
+			province: 'Provincia 3'
+		},
+		memberships: [
+			{
+				name: 'Membresía 1C',
+				monthDuration: 12
+			}
+		],
+		partnerType: 'Tipo C',
+		categories: [
+			{
+				name: 'Categoría C1',
+				description: 'Descripción C1'
+			},
+			{
+				name: 'Categoría C2',
+				description: 'Descripción C2'
+			}
+		],
+		phones: [
+			{
+				phoneNumber: '555-666-7777'
+			},
+			{
+				phoneNumber: '999-888-7777'
+			}
+		],
+		emails: [
+			{
+				email: 'correoC1@example.com'
+			},
+			{
+				email: 'correoC2@example.com'
+			}
+		],
+		websites: [
+			{
+				website: 'https://www.ejemploC1.com'
+			},
+			{
+				website: 'https://www.ejemploC2.com'
+			}
+		]
+	}
+];
+
+export const categories: any = [
+	{
+		name: 'Categoría A1',
+		description: 'Descripción A1'
+	},
+	{
+		name: 'Categoría A2',
+		description: 'Descripción A2'
+	},
+	{
+		name: 'Categoría B1',
+		description: 'Descripción B1'
+	},
+	{
+		name: 'Categoría C1',
+		description: 'Descripción C1'
+	},
+	{
+		name: 'Categoría C2',
+		description: 'Descripción C2'
+	}
+];
